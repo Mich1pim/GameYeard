1f218813023608f95c5a8eceedd7d5342b2a1a4c
Прохоров Илья Михайлович <prohorov.ilia2006@gmail.com>
2025-09-30 14:21:44 +0300

    5
    5

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -2,7 +2,34 @@ using Mono.Cecil.Cil;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Player
+public class Player : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 1.0f;
+
+    private PlayerInputActions playerInputActions;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Enable();
+    }
+
+
+    private Vector2 GetMovementVector()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        return inputVector;
+    }
+    void FixedUpdate()
+    {
+        Vector2 inputVector = GetMovementVector();
+
+        inputVector = inputVector.normalized;
+
+        rb.MovePosition(rb.position + inputVector * (moveSpeed * Time.fixedDeltaTime));
+    }
 
 }
